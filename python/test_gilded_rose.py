470d515406add81d53402743c26d9e60f444afcd
Anton P <antonp@draugiskisprendimai.lt>
2020-07-09 21:34:29 +0300

    [REF] refactoring and adding new tests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,11 +4,86 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_qty_denotion(self):
+        items = [Item("Scepter of Sargeras", 20, 30)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual(19, items[0].sell_in)
+        self.assertEqual(29, items[0].quality)
+
+    def test_qty_denotion_twoxfaster(self):
+        items = [Item("Ashbringer", 0, 30)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(28, items[0].quality)
+
+    def test_below_zero(self):
+        items = [Item("Scythe of Elune", 20, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+
+    def test_aged_brie(self):
+        items = [Item("Aged Brie", 5, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(11, items[0].quality)
+
+    def test_aged_brie_2(self):
+        items = [Item("Aged Brie", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(12, items[0].quality)
+
+    def test_aged_brie_quality_over_50(self):
+        items = [Item("Aged Brie", 10, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(50, items[0].quality)
+
+    def test_sulfurus(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 20, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(20, items[0].sell_in)
+        self.assertEqual(50, items[0].quality)
+
+    def test_sulfurus_past_saleout(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 0, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(50, items[0].quality)
+
+    def test_backstagepass_less_than_ten_days(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 9, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(22, items[0].quality)
+
+    def test_backstagepass_more_than_ten_days(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 20, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(21, items[0].quality)
+
+    def test_backstagepass_less_than_five_days_left(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 4, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(23, items[0].quality)
+
+    def test_backstagepass_over_fifty(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 4, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(50, items[0].quality)
+
+    def test_backstagepass_after_concert(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].quality)
+
 
 if __name__ == '__main__':
     unittest.main()
